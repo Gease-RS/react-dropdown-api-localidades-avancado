6e4257696725d8001ff1dd8d7fdf4036c684efe1
Gease-RS <geasebrasil@gmail.com>
2022-01-21 19:11:04 -0300

    Base inicial

---
+++ README.md
@@ -0,0 +1,27 @@
+<div align="center">
+  <p align="center">
+    <img src="src/assets/img/app-localidades.png" width="680" height="290" alt="React Cars App" />
+  </p>
+ <h1>App Dropdown Localidades IBGE </h1>
+ <h3>Projeto em Reactjs, consumindo Api do IBGE<h3>
+</div>
+
+# Passo a passo terminal
+
+1. Iniciando projeto do zero
+
+```bash
+    npx react-create-app react-dropdown-api-localidades
+```
+
+2. Entre no projeto criado
+
+```bash
+    cd react-dropdown-api-localidades
+```
+
+3. Comando para rodar aplicação
+
+```bash
+    npm start
+```
---
+++ src/App.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import DropdownBrazilianCities from "./components/Forms/DropdownBrazilianCities";
+import DropdownBrazilianStates from "./components/Forms/DropdownBrazilianStates";
+import "./App.css";
+
+function App() {
+  const [formValues, setFormValues] = useState({});
+
+  const handleInputChange = (e) => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  console.log("formValues", formValues);
+
+  return (
+    <div className="container">
+      <form>
+        <label htmlFor="state">Estado:</label>
+        <DropdownBrazilianStates onChange={handleInputChange} />
+        <label htmlFor="city">Cidade:</label>
+        <DropdownBrazilianCities
+          state={formValues.state}
+          onChange={handleInputChange}
+        />
+      </form>
+    </div>
+  );
+}
+
+export default App;
